0ed275d69a372e0e571d4ca96667e3920b1dcae0
Roman Kulichkov <roman@kulichkov.pro>
2016-08-12 19:21:34 +0700

    обновил файл конфигураций

--- local/php_interface/config/common.php
+++ local/php_interface/config/common.php
@@ -5,43 +5,21 @@
 \YT\Environment\EnvironmentManager::getInstance()->addConfig(
 	new \YT\Environment\Configuration\CommonConfiguration(
 		array(
-			'interiorIBlockId' => 1,
-			'exteriorIBlockId' => 2,
-			'feedbackIBlockId' => 3,
-			'feedbackPropPhoneId' => 1,
+			'sliderMainIBlockId' => 1,
+			// Заглушка для слайдера на главной
+			'sliderMainPlugId'   => 9999,
 		)
 	)
 );
 
 if(\Bitrix\Main\Loader::includeModule('itconstruct.resizer'))
 {
-    itc\Resizer::addPreset('interiorDetail', array(
+    itc\Resizer::addPreset('sliderMain', array(
             'mode' => 'width',
-            'width' => '1200',
+            'width' => '1280',
             'height' => null,
             'type' => 'jpg'
         )
     );
-    itc\Resizer::addPreset('interiorPreview', array(
-            'mode' => 'crop',
-            'width' => '200',
-            'height' => '168',
-            'type' => 'jpg'
-        )
-    );
-	itc\Resizer::addPreset('exteriorDetail', array(
-			'mode' => 'width',
-			'width' => '1024',
-			'height' => null,
-			'type' => 'jpg'
-		)
-	);
-	itc\Resizer::addPreset('exteriorPreview', array(
-			'mode' => 'crop',
-			'width' => '207',
-			'height' => '151',
-			'type' => 'jpg'
-		)
-	);
 }
 
