bbe058e131d7b6d30c5605d13c0fb2faddeeca19
Roman Kulichkov <roman@kulichkov.pro>
2016-08-12 18:50:11 +0700

    страница о нас

--- about/index.php
+++ about/index.php
@@ -1,31 +1,16 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetPageProperty("description", "Бутик-отель Морской Клуб (Sea Club Boutique Hotel) – приглашаем Вас на лучший элитный отдых на Черном море. Лучшие места для отдыха в окрестностях Геленджика возле моря. Бары, рестораны, бассейн, семейный отдых, морские прогулки, развлекательная программа.");
+$APPLICATION->SetPageProperty("TITLE", "Бутик-отель Морской Клуб (Sea Club Boutique Hotel) – приглашаем Вас на лучший элитный отдых на Черном море.");
 $APPLICATION->SetTitle("Бутик-отель Морской Клуб (Sea Club Boutique Hotel) – приглашаем Вас на лучший элитный отдых на Черном море");
 ?>
 <?$APPLICATION->IncludeComponent('bitrix:main.include', '',
     Array(
         'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/page_templates/pg_slider_main.php',
+        'PATH' => '/local/include/page_templates/pg_about.php',
         'EDIT_TEMPLATE' => ''
     ),
-    false
-);?>
-<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-    Array(
-        'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/page_templates/pg_banners_main.php',
-        'EDIT_TEMPLATE' => ''
-    ),
-    false
-);?>
-<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-    Array(
-        'AREA_FILE_SHOW' => 'file',
-        'PATH' => '/local/include/page_templates/pg_advantages_main.php',
-        'EDIT_TEMPLATE' => ''
-    ),
-    false
+    Array('HIDE_ICONS' => 'Y')
 );?>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
