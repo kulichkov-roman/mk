2a71454a49964ca40843501622137e9c530ccc16
Roman Kulichkov <roman@kulichkov.pro>
2016-08-12 19:29:15 +0700

    +шаблон сайта

--- local/templates/site_template/footer.php
+++ local/templates/site_template/footer.php
@@ -3,66 +3,39 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 	die();
 }
 ?>
-
 		</div> <!--/wrapper-->
-
-		<footer class="page-footer">
-			<div class="container">
-				<div class="footer-copyright">
-					<?$APPLICATION->IncludeComponent(
-						'bitrix:main.include',
-						'',
-						Array(
-							'AREA_FILE_SHOW' => 'file',
-							'PATH' => '/local/include/site_templates/ft_footer_copyright.php',
-							'EDIT_TEMPLATE' => ''
-						),
-						false
-					);?>
-				</div>
-				<div class="footer-developer">
-					<?$APPLICATION->IncludeComponent(
-						'bitrix:main.include',
-						'',
-						Array(
-							'AREA_FILE_SHOW' => 'file',
-							'PATH' => '/local/include/site_templates/ft_footer_developer.php',
-							'EDIT_TEMPLATE' => ''
-						),
-						false,
-						Array('HIDE_ICONS' => 'Y')
-					);?>
-				</div>
-			</div>
+		<div id='hotel-map' class='no-mobile'></div>
+		<footer>
+			<?$APPLICATION->IncludeComponent(
+				'bitrix:main.include',
+				'',
+				Array(
+					'AREA_FILE_SHOW' => 'file',
+					'PATH' => '/local/include/site_templates/ft_logo.php',
+					'EDIT_TEMPLATE' => ''
+				),
+				false
+			);?>
+			<?$APPLICATION->IncludeComponent(
+				'bitrix:main.include',
+				'',
+				Array(
+					'AREA_FILE_SHOW' => 'file',
+					'PATH' => '/local/include/site_templates/ft_social.php',
+					'EDIT_TEMPLATE' => ''
+				),
+				false
+			);?>
+			<?$APPLICATION->IncludeComponent(
+				'bitrix:main.include',
+				'',
+				Array(
+					'AREA_FILE_SHOW' => 'file',
+					'PATH' => '/local/include/site_templates/ft_contacts.php',
+					'EDIT_TEMPLATE' => ''
+				),
+				false
+			);?>
 		</footer>
-		<div class="hidden">
-			<div class="call-request-form" id="call-request">
-				<div class="call-request-form__wrap">
-					<div class="call-request-form__inner">
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/ft_call_request_form_3.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-					</div>
-				</div>
-			</div>
-			<div class="form-answer" id="form-answer">
-				<div class="form-answer__wrap">
-					<div class="form-answer__inner">
-						<div class="form-answer__title">Спасибо за заявку</div>
-						<div class="form-answer__text">
-							В самое ближайшее время с вами свяжется наш<br>
-							специалист и договорится с вами о просмотре.
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
 	</body>
 </html>
--- local/templates/site_template/header.php
+++ local/templates/site_template/header.php
@@ -26,7 +26,7 @@ IncludeTemplateLangFile(__FILE__);
 	$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . '/fonts/helvetica/stylesheet.css');
 	$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . '/css/developers.css');
 
-	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/modernizr-2.8.3.min');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/vendor/modernizr-2.8.3.min');
 
 	// is main page
 	$isMain = false;
@@ -35,12 +35,38 @@ IncludeTemplateLangFile(__FILE__);
 	}
 
 	$APPLICATION->AddHeadString('
-		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<script>
+            var map_contacts_lat=44.49168119827027;
+            var map_contacts_lng=38.1343515497681;
+            var map_contacts_zoom=12;
+            var map_contacts_from_lat=44.508675;
+            var map_contacts_from_lng=38.136072;
+        </script>
+
+	');
+	$APPLICATION->AddHeadString('
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script>window.jQuery || document.write(\''.SITE_TEMPLATE_PATH .'<script src="/js/vendor/jquery-1.11.3.min.js"><\/script>\')</script>
 	');
 
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/plugins.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/vendor/touchswipe.jquery.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/skyslider.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/jquery.fancybox.pack.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/swiper.min.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/css/fancybox/helpers/jquery.fancybox-buttons.js');
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/main.js');
 	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/developers.js');
-	?>
 
+	$APPLICATION->AddHeadString('
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAckPfKH6q-sp1kl2JaQih7CEFWMocSMf0&sensor=false"></script>
+	');
+	$APPLICATION->AddHeadString('
+		<!-- BEGIN CLICKTEX CODE {literal} -->
+			<script type="text/javascript" charset="utf-8" async="async" src="//www.clicktex.ru/code/26346"></script>
+		<!-- {/literal} END CLICKTEX CODE -->
+	');
+	?>
 	<?$APPLICATION->ShowHead()?>
 	<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
 		Array(
@@ -52,7 +78,6 @@ IncludeTemplateLangFile(__FILE__);
 	);?>
 </head>
 <body>
-
 	<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
 		Array(
 			'AREA_FILE_SHOW' => 'file',
@@ -61,8 +86,10 @@ IncludeTemplateLangFile(__FILE__);
 		),
 		false
 	);?>
-	<div class="header clearfix">
+	<div class="panel">
 		<?$APPLICATION->ShowPanel();?>
+	</div>
+	<div class="header clearfix">
 		<div class="header-logo">
 			<a href="" class="header-logo__link">
 				<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
@@ -107,378 +134,5 @@ IncludeTemplateLangFile(__FILE__);
 		</div>
 		<a href="javascript:void(0)" class="header__btn-menu-toggle js__header-menu-toggle"></a>
 	</div>
-	<div id='mobile-mode'></div>
-	<div id='wrapper'>
-	<?if($isMain){?>
-		<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-			Array(
-				'AREA_FILE_SHOW' => 'file',
-				'PATH' => '/local/include/site_templates/hd_slider_main.php',
-				'EDIT_TEMPLATE' => ''
-			),
-			false
-		);?>
-	<?}?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<div class="wrapper">
-		<header class="page-header">
-			<div class="container">
-				<div class="header-text">
-					<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-						Array(
-							'AREA_FILE_SHOW' => 'file',
-							'PATH' => '/local/include/site_templates/hd_header_text.php',
-							'EDIT_TEMPLATE' => ''
-						),
-						false
-					);?>
-				</div>
-				<div class="header-contacts">
-					<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-						Array(
-							'AREA_FILE_SHOW' => 'file',
-							'PATH' => '/local/include/site_templates/hd_header_phone.php',
-							'EDIT_TEMPLATE' => ''
-						),
-						false
-					);?>
-				</div>
-				<div class="header-logo">
-					<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-						Array(
-							'AREA_FILE_SHOW' => 'file',
-							'PATH' => '/local/include/site_templates/hd_header_logo.php',
-							'EDIT_TEMPLATE' => ''
-						),
-						false
-					);?>
-				</div>
-			</div>
-		</header>
-		<section class="content">
-			<section class="section section-promo">
-				<div class="container">
-					<div class="promo">
-						<div class="promo__wrap">
-							<div class="promo__inner">
-								<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_section_promo_h1.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-								<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_section_promo_text.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-								<div class="promo__action">
-									<a href="#call-request" class="btn btn_yellow btn_lg js__popUp">Записаться на просмотр</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section><!-- section about-->
-
-			<section class="section section-advantages">
-				<div class="container">
-					<div class="advantages">
-						<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_section_advantages_h2.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-						<?$APPLICATION->IncludeComponent('bitrix:main.include', '',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_section_advantages_list.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false,
-							Array('HIDE_ICONS' => 'Y')
-						);?>
-					</div>
-				</div>
-			</section> <!-- section-advantages -->
-			<section class="section section-offer">
-				<div class="container">
-					<div class="offer">
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_offer_info_h2.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-						<div class="offer-info">
-							<div class="offer-info__left">
-								<?$APPLICATION->IncludeComponent(
-									'bitrix:main.include',
-									'',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_offer_info_left.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-							</div>
-							<div class="offer-info__right">
-								<?$APPLICATION->IncludeComponent(
-									'bitrix:main.include',
-									'',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_offer_info_right.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-							</div>
-						</div>
-						<div class="offer__action">
-							<a href="#call-request" class="btn btn_yellow btn_lg js__popUp">Записаться на просмотр</a>
-						</div>
-						<div class="offer-gallery">
-							<?$APPLICATION->IncludeComponent(
-								'bitrix:main.include',
-								'',
-								Array(
-									'AREA_FILE_SHOW' => 'file',
-									'PATH' => '/local/include/site_templates/hd_offer_gallery.php',
-									'EDIT_TEMPLATE' => ''
-								),
-								false,
-								Array('HIDE_ICONS' => 'Y')
-							);?>
-						</div>
-					</div>
-				</div>
-			</section> <!-- section-offer -->
-			<section class="section section-about">
-				<div class="container">
-					<div class="about">
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_about_h2.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-						<div class="about-place">
-							<?$APPLICATION->IncludeComponent(
-								'bitrix:main.include',
-								'',
-								Array(
-									'AREA_FILE_SHOW' => 'file',
-									'PATH' => '/local/include/site_templates/hd_about_place_list.php',
-									'EDIT_TEMPLATE' => ''
-								),
-								false,
-								Array('HIDE_ICONS' => 'Y')
-							);?>
-						</div>
-					</div>
-					<div class="section-request section-request_dark">
-						<div class="call-request-form">
-							<div class="call-request-form__wrap">
-								<div class="call-request-form__inner">
-									<div class="form-request">
-										<?$APPLICATION->IncludeComponent(
-											'bitrix:main.include',
-											'',
-											Array(
-												'AREA_FILE_SHOW' => 'file',
-												'PATH' => '/local/include/site_templates/hd_section_request_form_1.php',
-												'EDIT_TEMPLATE' => ''
-											),
-											false,
-											Array('HIDE_ICONS' => 'Y')
-										);?>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="about-landscape">
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_about_landscape_h2.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_about_landscape_figure.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-						<div class="about-landscape__content">
-							<div class="about-landscape__descr">
-								<?$APPLICATION->IncludeComponent(
-									'bitrix:main.include',
-									'',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_about_landscape_descr.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-							</div>
-							<div class="about-landscape-text">
-								<?$APPLICATION->IncludeComponent(
-									'bitrix:main.include',
-									'',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_about_landscape_text.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section> <!-- section-about -->
-			<section class="section section-area">
-				<div class="container">
-					<div class="area">
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_section_area_h2.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-						<div class="area__wrap">
-							<div class="area-gallery">
-								<?$APPLICATION->IncludeComponent(
-									'bitrix:main.include',
-									'',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_section_area_gallery_list.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-							</div>
-							<div class="area__text">
-								<?$APPLICATION->IncludeComponent(
-									'bitrix:main.include',
-									'',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_section_area_text_h3.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-								<?$APPLICATION->IncludeComponent(
-									'bitrix:main.include',
-									'',
-									Array(
-										'AREA_FILE_SHOW' => 'file',
-										'PATH' => '/local/include/site_templates/hd_section_area_text_ul.php',
-										'EDIT_TEMPLATE' => ''
-									),
-									false
-								);?>
-							</div>
-						</div>
-
-					</div>
-				</div>
-			</section> <!-- section-area -->
-			<section class="section section-infrastructure">
-				<div class="container">
-					<div class="infrastructure">
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_section_infrastructure_h2.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-						<?$APPLICATION->IncludeComponent(
-							'bitrix:main.include',
-							'',
-							Array(
-								'AREA_FILE_SHOW' => 'file',
-								'PATH' => '/local/include/site_templates/hd_section_infrastructure_list.php',
-								'EDIT_TEMPLATE' => ''
-							),
-							false
-						);?>
-					</div>
-					<div class="section-request">
-						<div class="call-request-form">
-							<div class="call-request-form__wrap">
-								<div class="call-request-form__inner">
-									<?$APPLICATION->IncludeComponent(
-										'bitrix:main.include',
-										'',
-										Array(
-											'AREA_FILE_SHOW' => 'file',
-											'PATH' => '/local/include/site_templates/hd_section_request_form_2.php',
-											'EDIT_TEMPLATE' => ''
-										),
-										false,
-										Array('HIDE_ICONS' => 'Y')
-									);?>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section> <!-- section-infrastructure -->
-			<div class="section-map">
-				<div class="section-map__inner" id="map_canvas"></div>
-			</div>
-		</section> <!-- content -->
-	</div> <!-- wrapper -->
+	<div id="mobile-mode"></div>
+	<div id="wrapper">
