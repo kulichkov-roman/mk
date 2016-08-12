fdd8f157a165aa000d4dd27d835f383dd8a9563b
Roman Kulichkov <roman@kulichkov.pro>
2016-08-12 19:21:18 +0700

    +меню и блок с социальными сетями

--- local/include/site_templates/ft_social.php
+++ local/include/site_templates/ft_social.php
@@ -1,6 +1,11 @@
-
-				<div class='social'>
-					<a href='https://www.facebook.com/seaclubvip'><img src='img/facebook2x.png'></a>
-					<a href='https://www.instagram.com/seaclubvip'><img src='img/instagram2x.png'></a>
-					<a href='https://www.youtube.com/channel/UCJ3ckXXzdwC9GWAPCSnHOSQ'><img src='img/youtube2x.png'></a>
-				</div>
+<div class='social'>
+	<a href='https://www.facebook.com/seaclubvip'>
+		<img src='/local/images/facebook2x.png'>
+	</a>
+	<a href='https://www.instagram.com/seaclubvip'>
+		<img src='/local/images/instagram2x.png'>
+	</a>
+	<a href='https://www.youtube.com/channel/UCJ3ckXXzdwC9GWAPCSnHOSQ'>
+		<img src='/local/images/youtube2x.png'>
+	</a>
+</div>
--- local/include/site_templates/hd_menu.php
+++ local/include/site_templates/hd_menu.php
@@ -1,11 +1,19 @@
-
-			<nav>
-				<ul>
-					<li ><a href='about'>Об отеле</a></li>
-					<li ><a href='rooms'>Номера</a></li>
-					<li ><a href='restaurant'>Ресторан</a></li>
-					<li ><a href='gallery'>Фотогалерея</a></li>
-					<li ><a href='events'>Мероприятия</a></li>
-					<li ><a href='offers'>Цены</a></li>
-				</ul>
-			</nav>
+<?
+// Меню - http://dev.1c-bitrix.ru/user_help/settings/settings/components_2/navigation/menu.php
+$APPLICATION->IncludeComponent(
+	"bitrix:menu",
+	"main-menu",
+	Array(   
+		"ROOT_MENU_TYPE"           => "top",     // Тип меню для первого уровня
+		"MAX_LEVEL"                => "1",        // Уровень вложенности меню    
+		"CHILD_MENU_TYPE"          => "top",     // Тип меню для остальных уровней
+		"USE_EXT"                  => "N",        // Подключать файлы с именами вида .тип_меню.menu_ext.php    
+		"DELAY"                    => "N",        // Откладывать выполнение шаблона меню    
+		"ALLOW_MULTI_SELECT"       => "N",        // Разрешить несколько активных пунктов одновременно    
+		"MENU_CACHE_TYPE"          => "N",        // Тип кеширования    
+		"MENU_CACHE_TIME"          => "3600",     // Время кеширования (сек.)    
+		"MENU_CACHE_USE_GROUPS"    => "Y",        // Учитывать права доступа    
+		"MENU_CACHE_GET_VARS"      => "",         // Значимые переменные запроса 
+	)
+);
+?>
