236b2816e98d1b0fd044bbbc1d09ec9402a9b0cb
Roman Kulichkov <roman@kulichkov.pro>
2016-08-12 19:23:42 +0700

    +шаблон меню

--- local/templates/site_template/components/bitrix/menu/main-menu/template.php
+++ local/templates/site_template/components/bitrix/menu/main-menu/template.php
@@ -1,16 +1,12 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?if (!empty($arResult)){?>
-	<nav class="menu">
-		<ul class="menu__list">
+	<nav>
+		<ul>
 			<?foreach($arResult as $arItem){?>
 				<?if ($arItem["PERMISSION"] > "D"){?>
-					<li class="menu__item <?=$arItem['SELECTED'] ? '_active' : ''?>">
-						<a href="<?=$arItem["LINK"]?>" class="menu__link">
-							<span class="menu__text">
-								<?=$arItem["TEXT"]?>
-							</span>
-						</a>
+					<li class="<?=$arItem['SELECTED'] ? 'active' : ''?>">
+						<a href='<?=$arItem["LINK"]?>'><?=$arItem["TEXT"]?></a>
 					</li>
 				<?}?>
 			<?}?>
